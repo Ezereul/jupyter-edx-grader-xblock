ea57ef7bebe2fcaa8d81eb1cc6ae2212a677b1be
Maxim Isaev <isaev_0602@mail.ru>
2024-07-12 16:10:08 +0500

    urls update

--- xblock_jupyter_graded/rest/urls.py
+++ xblock_jupyter_graded/rest/urls.py
@@ -1,7 +1,7 @@
 """
 Defines a URL to return a notebook html page to be used in an iframe
 """
-from django.conf.urls import url
+from django.urls import re_path
 
 from xblock_jupyter_graded.rest.views import (
     DownloadStudentNBView, DownloadInstructorNBView, DownloadAutogradedNBView
@@ -11,17 +11,17 @@ from django.contrib.auth.decorators import login_required
 app_name = 'xblock_jupyter_graded'
 
 urlpatterns = [
-    url(
+    re_path(
     r'^download/student_nb/(?P<course_id>.+)/(?P<unit_id>.+)/(?P<filename>.+)$',
         login_required(DownloadStudentNBView.as_view()),
         name='jupyter_student_dl'
     ),
-    url(
+    re_path(
     r'^download/instructor_nb/(?P<course_id>.+)/(?P<unit_id>.+)/(?P<filename>.+)$',
         login_required(DownloadInstructorNBView.as_view()),
         name='jupyter_instructor_dl'
     ),
-    url(
+    re_path(
     r'^download/autograded_nb/(?P<course_id>.+)/(?P<unit_id>.+)/(?P<filename>.+)$',
         login_required(DownloadAutogradedNBView.as_view()),
         name='jupyter_autograded_dl'
